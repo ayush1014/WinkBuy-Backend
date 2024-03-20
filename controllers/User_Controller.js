c6c906ed17a4a2a5610be470f3e5ff90221489ed
ayush1014 <60238194+ayush1014@users.noreply.github.com>
2024-03-20 13:30:22 -0500

    added trash feature in wishlist

--- controllers/User_Controller.js
+++ controllers/User_Controller.js
@@ -124,7 +124,7 @@ exports.viewProductById = async (req, res) => {
 // Controller to get all products
 exports.viewAllProducts = async (req, res) => {
     const page = parseInt(req.query.page) || 1; 
-    const limit = parseInt(req.query.limit) || 4; 
+    const limit = parseInt(req.query.limit) || 6; 
     const offset = (page - 1) * limit;
 
     try {
