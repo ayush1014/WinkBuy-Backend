b61a9a5567029747faae0e39eaac8992198112c1
ayush1014 <ayushkanaujia14@gmail.com>
2025-08-04 03:57:49 -0400

    added new deps

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
-const {Sequelize} = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 require('dotenv').config();
 
 const db = new Sequelize({
-    username:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host:process.env.DB_HOST,
-    dialect:'mysql',
-    port:process.env.DB_PORT
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    port: process.env.DB_PORT
 })
 
 module.exports = db;
